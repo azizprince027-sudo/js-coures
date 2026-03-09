16c0392ae1d7bab02832e30eb062943838e0f536
Odjo Aziz <azizprince027@gmail.com>
2026-03-09 15:30:29 +0000

    projet js

---
+++ index.js
@@ -0,0 +1,141 @@
+// let a = prompt("entre un nombre pour verifier ta sexualiter");
+
+// if(a % 2=== 0)  {
+// alert ("woubie");
+
+// }
+// else {
+//     alert("pd chien la");
+// }
+//pour comprendre la commande return
+// function direBonjour() {
+//     console.log("Bonjour !"); 
+// }
+
+// let cadeau = direBonjour(); 
+// console.log(cadeau); 
+
+// function donnerBonjour() {
+//     return "Bonjour !"; 
+// }
+
+// let cadeau = donnerBonjour(); 
+// console.log(cadeau); 
+
+// fonction calcul qui  affiche le resultat
+
+// function calcul(a, b) {
+//     return a + b;
+
+// }
+
+// let resultat = calcul(7, 9);
+// console.log(resultat);
+
+// function faireSandwich() {
+//    return "mange"
+// }
+
+// let donne = faireSandwich();
+
+// function demanderAge() {
+//     let age = prompt("saisissez votre age");
+//     alert("vous avez "  + age  +  "ans " );
+// }demanderAge();
+
+// function prevoireAge() {
+//     let age = prompt("saisissez votre age");//vue que c chaine de caractere ca concane tout
+//   age = parseInt (age); //sert a convertir chaine en nombre
+//     alert("vous aurez "  + (age +1)  +  "ans " );
+// }
+
+// (function () {console.log ("bisous")})(); auto exeecuter
+  
+// let functionanonyme =function() {
+//     console.log("bious");
+
+// }
+// functionanonyme ();
+
+
+// let  age =  prompt ("saisire ton age");
+
+// if (age < 18 ) {
+//     alert("vous n est pas majeure");
+// }
+
+// else if (age >= 21 ) {
+//     alert("vous etes majeure partout a vous les casino");
+// }
+// else {
+//     alert("vous ete majeure en france");
+// }
+
+// let x = 3 
+
+// if (x>3) (
+// console.log ("x est grand")
+
+// )
+
+// else {
+// console.log("x est petit")
+
+// }
+
+// condition tener :  [condition] ? [true] : [false];
+
+// let x=5  
+// x>3 ? console.log("x est grand") : console.log("x est petit"); c comme ca  on fait une condition tener
+
+// let i = 1;
+
+// while (i < 5) {
+
+// console.log("ligne :" + i);
+// i++;
+// } sans le i ++ on a une boucle infinie
+
+
+
+// do {
+// var prnom = prompt("ton prenom");
+// } while(prnom == ""  || prnom == null)
+
+// alert ( "bonjour  " + prnom); on a utiliser  une var globale
+
+// fonction recursive .  
+
+// function timer(secondes) { //secondes vaut 10
+//     if (secondes > 0) {
+//         console.log(secondes);
+//         timer(secondes -1) // 10 -1 =9
+
+//         // afficher timrs  9 -1
+//     }
+//     else {
+//         console.log(secondes)
+//     }
+// }
+
+// timer(10);
+
+// les tableau
+
+// let tableau [elements];
+
+// tableau 2 dimention
+
+// let tableau = [
+
+//         ["elements1"],
+//         ["elements"]
+//     ];
+
+// let mon tableau asso = {    tableau asso 
+// "prenom" : "mark",
+// "nom"    : "zuk",
+// "poste"  : "dg"
+
+
+// };
